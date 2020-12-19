31f543e23216b4bb644d4c9562d94eea7ea5f5e1
jakhar1996 <hi@oneartboard.com>
2020-12-20 00:11:33 +0530

    deploy

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
